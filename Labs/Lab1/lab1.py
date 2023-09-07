0f2dd25a7c38346ec0eeb00cc8085e8b03d1c65c
Luke <luse9638@colorado.edu>
2023-09-07 14:45:51 -0600

    Added comments to Lab1

--- Labs/Lab1/lab1.py
+++ Labs/Lab1/lab1.py
@@ -7,10 +7,12 @@ y = np.arange(0, 100, 20)
 
 print("The first three entries of x are: " + str(x[0:3]))
 
+# creating w values
 w = 10 ** (-np.linspace(1, 10, 10))
 print(w)
 x = np.arange(1, len(w) + 1, 1)
 print(x)
+# semilogx/y use a log scale on that respective axis
 plt.semilogy(x, w)
 s = 3 * w
 plt.semilogy(x, s)
