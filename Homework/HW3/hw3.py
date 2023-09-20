587a1754cd2fe57dfdb4cb642e44870a732b8851
Luke <luse9638@colorado.edu>
2023-09-20 16:46:53 -0600

    Problem 1c

--- Homework/HW3/hw3.py
+++ Homework/HW3/hw3.py
@@ -1,26 +1,37 @@
+######################################################################## Imports
+
+import numpy as np
+
 #################################################################### Subroutines
 
-# create bisection subroutine
 # wrote this instead of downloading the one on canvas because...
-def bisection(a, b, f, tol):
+def bisection(a, b, f, tol, *args):
     """
-    Find root of function f on interval (a, b)
-        Inputs:
-            a: left endpoint
-            b: right endpoint
-            f: function to find roots of
-            tol: desire minimum interval size containing a root
+    Find root of function f on interval (a, b) \n
+        Inputs: \n
+            a: left endpoint \n
+            b: right endpoint \n
+            f: function to find roots of \n
+            tol: desire minimum interval size containing a root \n
+            realRoot: (optional) will instead run until distance between
+            approximated and real root is less than tol
+            
         
         Outputs:
+            (c, err, count), where:
             c: approximation of root, where f(c) ~= 0
-            err: 0 if successful, 1 if failure
+            err: 0 if successful, 1 if no sign change
+            count: number of iterations run
     """
 
+    # keep track of total iterations
+    count = 0
+    
     # if no sign changes occurs, bisection won't work
     if ((f(a) * f(b)) >= 0): 
         c = b
         err = 1
-        return ("Err: " + str(err), "Root: " + str(c))
+        return (c, err, count)
 
     # now that we know a sign change occurs we can proceed
     
@@ -30,8 +41,18 @@ def bisection(a, b, f, tol):
     fa = f(a)
     fd = f(d)
 
-    # continue until interval is within tolerance
-    while (abs(b - a) > tol):
+    # if actual root passed to function, switch to making sure distance between
+    # approximation and real root is within tolerance rather than having the
+    # interval be within tolerance
+    if args:
+        realRoot = args[0]
+        print("Real root entered: " + str(realRoot))
+        condition = abs(realRoot - d)
+    else:
+        condition = abs(b - a)
+
+    # continue until interval or is within tolerance
+    while (condition > tol):
         # root is actually in right interval
         if ((fd * fa) > 0):
             # move left endpoint to midpoint
@@ -45,11 +66,32 @@ def bisection(a, b, f, tol):
         d = 0.5 * (a + b)
         fd = f(d)
 
+        # recalculate condition
+        if args:
+            condition = abs(realRoot - d)
+        else:
+            condition = abs(b - a)
+
+        # update iteration counter
+        count += 1
+
+        # track our progress
+        print("Iterations ran: " + str(count) + ", Current approximation: "\
+               + str(d), end = '\r')
+
     # once loop ends, return root approximation
+    print("")
     c = d
     err = 0
-    return ("Err: " + str(err), "Root: " + str(c))
+    return (c, err, count)
 
 
 ############################################################################# 1)
 ################################## c)
+
+# define our function
+f = lambda x: np.sin(x) - (2 * x) + 1
+# tolerance
+tolerance = 1 * 10 ** -5
+(r, error, iterations) = bisection(0, 1, f, tolerance, 0.888)
+print(r)
