ab27556145a9d4eda3ce5acd27b5b861add7c91c
Luke <luse9638@colorado.edu>
2023-09-26 16:29:13 -0600

    Problem 1c)

--- Homework/HW4/hw4.py
+++ Homework/HW4/hw4.py
@@ -15,8 +15,7 @@ def bisection(a, b, f, tol, *args):
             f: function to find roots of \n
             tol: desire minimum interval size containing a root \n
             realRoot: (optional) will instead run until distance between
-            approximated and real root is less than tol \n
-            
+            approximated and real root is less than tol \n  
         Outputs: \n
             (c, err, count), where: \n
                 c: approximation of root, where f(c) ~= 0 \n
@@ -84,6 +83,34 @@ def bisection(a, b, f, tol, *args):
     err = 0
     return (c, err, count)
 
+def fixedpt(f, x0, tol, Nmax):
+    """
+    Run a fixed point iteration f on (a, b) \n
+        Inputs: \n
+            f: fixed point iteration \n
+            x0: initial guess \n
+            Nmax: max iterations to run \n
+            tol: stopping tolerance \n 
+        Outputs: \n
+            (xstar, ier) where: \n
+                xstar: approximation of fixed point \n
+                ier: 0 if successful, 1 if max iterations reached \n
+    """
+
+    count = 0
+    while (count < Nmax):
+       count = count + 1
+       x1 = f(x0)
+       if (abs(x1 - x0) / abs(x0) < tol):
+          xstar = x1
+          ier = 0
+          return (xstar, ier)
+       x0 = x1
+
+    xstar = x1
+    ier = 1
+    return (xstar, ier)
+
 ############################################################################# 1)
 ################################### a)
 
@@ -92,26 +119,53 @@ alpha = 0.138 * 10 ** -6
 t_f = 5184000
 T_i = 20
 T_s = -15
-# 500 x-values, x between 0 and 1
-x = np.linspace(0, 1, 500)
+
 # f(x) = erf(x / 2sqrt(alpha * t_f)) + (T_s) / (T_i - T_s)
 f = lambda x: sp.special.erf((x) / (2 * np.sqrt(alpha * t_f))) +\
 ((T_s) / (T_i - T_s))
+# f'(x)
+dfdx = lambda x: ((1) / (np.sqrt(np.pi * alpha * t_f))) *\
+    np.exp((-1 * x**2) / (4 * alpha * t_f))
+
+# 500 points of (x, f(x))
+x = np.linspace(0, 1, 500)
 y = f(x)
+
 # y0 = 0
 y0 = np.zeros([len(x)])
+
 # plot!
 plt.plot(x, y)
 plt.plot(x, y0)
 plt.title("Problem 1a)")
-# plt.show()
+plt.show()
 
 ################################### b)
+print("")
+print("Problem 1b)")
+
 # tolerance
 tolerance = 10 ** -13
+
 # run bisection on interval [0, 1]
 (root, error, iterations) = bisection(0, 1, f, tolerance)
-print("Problem 1b)")
-print("Approximate depth: " + str(root))
+print("Error code: " + str(error))
+
+################################### c)
+print("")
+print("Problem 1c)")
+
+# Newton's method iteration
+n = lambda x: x - (f(x) / dfdx(x))
+
+# run Newton's method with x0 = 0.01
+(newtRoot1, error) = fixedpt(n, 0.01, 1000, tolerance)
+print("Approximate depth with x0 = 0.01: " + str(newtRoot1))
+print("Error code: " + str(error))
+
+# run Newton's method with x0 = 1
+(newtRoot2, error) = fixedpt(n, 1, 1000, tolerance)
+print("Approximate depth with x0 = 1: " + str(newtRoot2))
+print("Error code: " + str(error))
 
 
