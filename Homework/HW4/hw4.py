a10433a040e6a778f595eee6a7c174d29932d5a6
Luke <luse9638@colorado.edu>
2023-09-26 15:50:12 -0600

    Problem 1a)

---
+++ Homework/HW4/hw4.py
@@ -0,0 +1,25 @@
+######################################################################## imports
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+############################################################################# 1)
+################################### a)
+
+# constants
+alpha = 0.138 * 10 ** -6
+t_f = 60 #5184000
+T_i = 20
+T_s = -15
+# 500 x-values, x between 0 and 0.005
+x = np.linspace(0, 0.005, 500)
+# y = f(x) = erf(x / 2sqrt(alpha * t_f)) + (T_s) / (T_i - T_s)
+y = sp.special.erf((x) / (2 * np.sqrt(alpha * t_f))) + (((T_s)) / (T_i - T_s))
+# y0 = 0
+y0 = np.zeros([len(x)])
+# plot!
+plt.plot(x, y)
+plt.plot(x, y0)
+plt.title("Problem 1a)")
+plt.show()
