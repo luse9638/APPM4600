d2c08f15338a967a871c62e58f2c4d2442cc4842
Luke <luse9638@colorado.edu>
2023-09-26 15:58:03 -0600

    Fixed error in 1a)

--- Homework/HW4/hw4.py
+++ Homework/HW4/hw4.py
@@ -9,11 +9,11 @@ import matplotlib.pyplot as plt
 
 # constants
 alpha = 0.138 * 10 ** -6
-t_f = 60 #5184000
+t_f = 5184000
 T_i = 20
 T_s = -15
-# 500 x-values, x between 0 and 0.005
-x = np.linspace(0, 0.005, 500)
+# 500 x-values, x between 0 and 1
+x = np.linspace(0, 1, 500)
 # y = f(x) = erf(x / 2sqrt(alpha * t_f)) + (T_s) / (T_i - T_s)
 y = sp.special.erf((x) / (2 * np.sqrt(alpha * t_f))) + (((T_s)) / (T_i - T_s))
 # y0 = 0
